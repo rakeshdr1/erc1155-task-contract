e44a328a2b56eb8b9c44c3d413e4cf46afe9e102
Rakesh D R <rakesh@rapidinnovation.dev>
2022-09-29 11:10:53 +0530

    added generated abi

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,25 @@
-import { ethers } from "hardhat";
+const { ethers, network, artifacts } = require("hardhat");
+const fs = require("fs");
+const path = require("path");
+
+function saveContractABI(contract: any, artifactName: string) {
+  const contractsDir = path.resolve(__dirname, "../output");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    `${contractsDir}/${artifactName}-${network.name}.json`,
+    JSON.stringify({ [artifactName]: contract.address }, undefined, 2)
+  );
+  const Artifact = artifacts.readArtifactSync(artifactName);
+
+  fs.writeFileSync(
+    `${contractsDir}/${artifactName}.json`,
+    JSON.stringify(Artifact, null, 2)
+  );
+}
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -10,6 +31,7 @@ async function main() {
   console.log(
     `TaskContract deployed to ${task.address} from Address ${deployer.address}`
   );
+  saveContractABI(task, "TaskContract");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
